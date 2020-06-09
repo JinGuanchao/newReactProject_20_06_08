c3dd9e9c120a1e96496017d6d952b7c16710605a
Arthur <jgc14115@163.com>
2020-06-09 17:29:58 +0800

    调整版本200609

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
+// import logo from './logo.svg';
 import './App.css';
+import Home from './router/Home';
+import About from './router/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  render(){
+    return(
+          <div className="App">
+              <BrowserRouter>
+                  {/* <HashRouter> */}
+                    <Switch>
+                      <Route exact component={About} path="/about"/>
+                      <Route exact component={Home} path="/"/>
+                    </Switch>
+                  {/* </HashRouter> */}
+                </BrowserRouter>
+          {/* <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              这里是 <code>React</code> 自有项目.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              血小板~
+            </a>
+          </header> */}
+        </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/router/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Home extends React.Component{
+    constructor(){
+        super();
+        this.state={}
+    }
+    render(){
+        return(
+            <div>
+                <div>Home</div>
+            </div>
+        )
+    }
+}
+
+export default Home;
